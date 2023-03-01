edb60ef55b851344ee25524c7419e9a63c835cb5
Sophie <bloxinas@list.ru>
2023-03-01 17:33:50 +0700

    исправлена функция reset

--- 2.js
+++ 2.js
@@ -5,32 +5,33 @@ const FIELD_SIZE = 10;
 var audio;
 document.getElementById('start').style.display = 'inline';
 
-function deepEqual([x1, y1], [x2, y2]) {
+function deepEqual([x1, y1], [x2, y2]) { // передается пара координат для сравнения, если первый и второй элемент равны - возвращаем true
     if (x1 === x2 && y1 === y2) return true;
     return false;
 }
 
-function createElement(name, x, y, field) {
+function createElement(name, x, y, field) { // создаем контейнер, задаем класс - корабль, количество палуб и положение (''/'вертикальное')
     const div = document.createElement('div');
     div.className = name;
-    div.style.cssText = `margin-left:${(x + 1) * 40}px; margin-top:${(y + 1) * 40}px;position: fixed;`;
-    document.getElementById(field).appendChild(div);
+    div.style.cssText = `margin-left:${(x + 1) * 40}px; margin-top:${(y + 1) * 40}px;position: fixed;`; // одна клетка - 40 пикселей,
+    // задаем стиль - отступ от левого верхнего угла на x + 1, т.к. верхняя и левая строки - шапки полей
+    document.getElementById(field).appendChild(div); // передаем, к какому полю необходимо добавить элемент, и добавляем его
 }
 
-function replace(coord, value) {
+function replace(coord, value) { // когда рассчитываем диагональные элементы, если вышли за пределы - заменяем на нужное значение
     if (coord[0] < 0 || coord[0] > 9) coord[0] = value;
     if (coord[1] < 0 || coord[1] > 9) coord[1] = value;
 }
 
-function playAudio(path) {
+function playAudio(path) { // загружаем аудио и воспроизводим
 
     audio = new Audio(path);
     audio.play();
 }
 
-function stopAudio() {
+function stopAudio() { // выключить музыку
     audio.pause();
-    audio.currentTime = 0;
+    audio.currentTime = 0; // устанавливаем бегунок музыки на 0 сек.
 }
 class Field {
     name;
@@ -38,11 +39,11 @@ class Field {
     field = [];
     crushedShips = 0;
     constructor(name) {
-        this.name = name;
+        this.name = name; // передаем имя поля div элемента
         for (let row = 0; row < FIELD_SIZE; row++) {
             this.field[row] = [];
             for (let col = 0; col < FIELD_SIZE; col++) {
-                this.field[row][col] = 0;
+                this.field[row][col] = 0; // инициализируем массив выстрелов по полю 0
             }
         }
     }
@@ -53,14 +54,14 @@ class Field {
                 let isCorrect = true;
                 do {
                     isCorrect = true;
-                    var ship = new Ship(i);
+                    var ship = new Ship(i); // создаем корабль - передаем размер
                     trys++;
                     if (trys > 300) {
-                        this.ships = [];
-                        return false;;
+                        this.ships = []; // если не получается создать поле с уже сгенерированными кораблями - сбрасываем значения кораблей
+                        return false; // возвращаем false
                     }
                     for (let k = 0; k < this.ships.length; k++) {
-                        if (this.ships[k].isConnectTo(ship)) {
+                        if (this.ships[k].isConnectTo(ship)) { // проверяем, граничит ли сгенерированный корабль с другими уже созданными
                             isCorrect = false;
                             break;
                         }
@@ -74,15 +75,17 @@ class Field {
                         }
                     });*/
 
-                } while (isCorrect === false);
-                this.ships.push(ship);
+                } while (isCorrect === false); // пока подходящий корабль не сгенерируется - цикл будет работать
+                this.ships.push(ship); // добавляем объект корабля в массив
             }
         }
         return true;
     }
     reset() {
         this.ships = [];
-        document.getElementById(this.name).innerHTML = '';
+        this.field = [];
+        crushedShips = 0;
+        document.getElementById(this.name).innerHTML = ''; // очищаем дочерние элементы контейнера
     }
     showShips() {
         name = this.name;
@@ -90,8 +93,8 @@ class Field {
             item.show(name);
         });
     }
-    hit(x, y) {
-        let col = Math.floor(x / 40) - 1;
+    hit(x, y) { // возвращаем значение в зависимости от того, был ли выстрел, и был ли он удачным
+        let col = Math.floor(x / 40) - 1; // получаем значение столбца и строки - координаты относительного левого угла окна делим на 1 клетку и вычитаем 1 (из-за шапки)
         let row = Math.floor(y / 40) - 1;
         if (col < 0 || row < 0) return;
         if (this.field[row][col] === 1) {
@@ -99,7 +102,7 @@ class Field {
             return 0;
         }
         this.field[row][col] = 1;
-        for (let i = 0; i < this.ships.length; i++) {
+        for (let i = 0; i < this.ships.length; i++) { // если не попал - ставим точку, попал - крестик, убил - все клетки рядом закрываются
             if (this.ships[i].isHit(row, col)) {
                 createElement('cross', col, row, this.name);
                 if (this.ships[i].isAllHit()) {
@@ -132,7 +135,7 @@ class Ship {
     startCoord;
     finishCoord;
     coordinate = [];
-    constructor(size) {
+    constructor(size) { // генериурем координаты, направление
         this.size = size;
         this.direction = Math.floor(Math.random() * 2);
         if (this.direction === 1) {
@@ -154,7 +157,8 @@ class Ship {
         replace(this.finishCoord, 9);
 
     }
-    closeAll(fieldName, grid) {
+    closeAll(fieldName, grid) { // закрываем клетки рядом с кораблем при его потоплении
+        // проверяем при помощи диагональных значений координат корабля - левый верхний и правый нижний угол "прямоугольника"
         if (this.direction === 1) {
             for (let y = this.startCoord[1]; y <= this.finishCoord[1]; y++) {
                 for (let x = this.startCoord[0]; x <= this.finishCoord[0]; x++) {
@@ -189,7 +193,7 @@ class Ship {
         let name = 'ship deck' + this.size + dir;
         createElement(name, this.col, this.row, fieldName);
     }
-    isHit(row, col) {
+    isHit(row, col) { // проверяем, совпадают ли координаты выстрела и координаты каждой клетки корабля
         for (let i = 0; i < this.size; i++) {
             if (deepEqual(this.coordinate[i], [row, col])) {
                 this.hits++;
@@ -202,7 +206,7 @@ class Ship {
         if (this.hits === this.size) return true;
         return false;
     }
-    isConnectTo(ship) {
+    isConnectTo(ship) { // проверяем, касаются ли поля кораблей друг друга также при помощи диагональных координат
         if (Math.abs(this.startCoord[0] - ship.startCoord[0]) > 1 && Math.abs(ship.finishCoord[0] - this.finishCoord[0]) > 1 || (Math.abs(this.startCoord[1] - ship.startCoord[1]) > 1 && Math.abs(ship.finishCoord[1] - this.finishCoord[1]) > 1)) {
             if ((ship.startCoord[0] < this.finishCoord[0] && Math.abs(ship.startCoord[1] - this.startCoord[1]) < 2) || (this.startCoord[0] > ship.finishCoord[0] && Math.abs(this.startCoord[1] - ship.startCoord[1]) < 2)) return true;
             if ((ship.startCoord[1] < this.finishCoord[1] && Math.abs(ship.startCoord[0] - this.startCoord[0]) < 2) || (this.startCoord[1] > ship.finishCoord[1] && Math.abs(this.startCoord[0] - ship.startCoord[0]) < 2)) return true;
